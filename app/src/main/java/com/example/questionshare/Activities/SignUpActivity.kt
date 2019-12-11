67aa856d06d3152668bd828f9011814049c2c4e5
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 21:48:26 +0600

    added sign up activity

---
+++ app/src/main/java/com/example/questionshare/Activities/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.questionshare.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.questionshare.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
