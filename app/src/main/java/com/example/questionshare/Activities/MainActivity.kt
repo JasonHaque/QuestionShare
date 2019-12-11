c746d072fc9bde7587e79b9839ee068023df7eb8
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 21:56:36 +0600

    gradle scripts added

--- app/src/main/java/com/example/questionshare/Activities/MainActivity.kt
+++ app/src/main/java/com/example/questionshare/Activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.questionshare.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.questionshare.R
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this,LoginActivity::class.java))
 
     }
 }
