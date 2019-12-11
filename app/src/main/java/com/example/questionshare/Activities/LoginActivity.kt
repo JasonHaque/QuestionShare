d7aa6af8f1881af7023a8ec34a8da9894c5caddf
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 22:29:08 +0600

    added validator methods

--- app/src/main/java/com/example/questionshare/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/questionshare/Activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.questionshare.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.questionshare.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -19,5 +20,26 @@ class LoginActivity : AppCompatActivity() {
             val intent= Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+        logInButton.setOnClickListener {
+            val email=LoginMail.text.toString()
+            val pass=LoginPassword.text.toString()
+            val validator=validateInputs(email,pass)
+            if(validator==false){
+                toastify("Fill up the fields properly")
+                return@setOnClickListener
+            }
+
+        }
+    }
+    fun validateInputs(email:String,pass:String): Boolean{
+
+        if(email.isEmpty() || pass.isEmpty()){
+            return false
+
+        }
+        return true
+    }
+    fun toastify(message:String){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
     }
 }
