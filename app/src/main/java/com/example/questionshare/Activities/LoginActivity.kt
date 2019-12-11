82d26ceeb1eb72ed2da552aecd8f48b55f318a26
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 22:00:39 +0600

    added sign up laypout

--- app/src/main/java/com/example/questionshare/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/questionshare/Activities/LoginActivity.kt
@@ -1,13 +1,23 @@
 package com.example.questionshare.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.questionshare.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        bindListeners()
+    }
+
+    fun bindListeners(){
+        GotoSignUpButton.setOnClickListener {
+            val intent= Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
