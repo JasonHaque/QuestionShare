014fe32f177e3fe72754972d0c5143f5a8b17697
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 22:11:16 +0600

    added validation code

--- app/src/main/java/com/example/questionshare/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/questionshare/Activities/SignUpActivity.kt
@@ -2,12 +2,35 @@ package com.example.questionshare.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.questionshare.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        bindListeners()
+    }
+    fun bindListeners(){
+        SignUpButton.setOnClickListener {
+            val validator =validateInputs()
+            if(!validator){
+                Toast.makeText(this,"Fill up the fields properly",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+        }
+    }
+    fun validateInputs(): Boolean{
+        val email=SignUPMail.text.toString()
+        val pass=SignUpPassword.text.toString()
+        if(email.isEmpty() || pass.isEmpty()){
+            return false
+
+        }
+        return true
     }
 }
