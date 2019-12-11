c12f04ea431a7a6597e2b41e869a2ad00d5e6faa
JasonHaque <zahinjason220434@gmail.com>
2019-12-11 22:03:33 +0600

    added profile view

---
+++ app/src/main/java/com/example/questionshare/Views/ProfileView.kt
@@ -0,0 +1,13 @@
+package com.example.questionshare.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.questionshare.R
+
+class ProfileView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_view)
+    }
+}
