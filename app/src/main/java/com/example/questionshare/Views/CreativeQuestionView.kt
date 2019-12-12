6e72b2285320fc1ccb121996f6ef4de6456729a6
JasonHaque <zahinjason220434@gmail.com>
2019-12-12 18:00:35 +0600

    added creative question view

---
+++ app/src/main/java/com/example/questionshare/Views/CreativeQuestionView.kt
@@ -0,0 +1,13 @@
+package com.example.questionshare.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.questionshare.R
+
+class CreativeQuestionView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creative_question_view)
+    }
+}
