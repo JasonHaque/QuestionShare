9b565a25e84e080b1f1823eb441c857bc3f36f12
JasonHaque <zahinjason220434@gmail.com>
2019-12-12 18:02:26 +0600

    added new view

---
+++ app/src/main/java/com/example/questionshare/Views/MultipleChoiceQuestionView.kt
@@ -0,0 +1,13 @@
+package com.example.questionshare.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.questionshare.R
+
+class MultipleChoiceQuestionView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multiple_choice_question_view)
+    }
+}
