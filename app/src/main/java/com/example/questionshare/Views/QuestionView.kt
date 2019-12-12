61b5ff3225ce2039f01deec9760adb5969f45315
JasonHaque <zahinjason220434@gmail.com>
2019-12-12 18:25:00 +0600

    added new base layout

--- app/src/main/java/com/example/questionshare/Views/QuestionView.kt
+++ app/src/main/java/com/example/questionshare/Views/QuestionView.kt
@@ -1,13 +1,27 @@
 package com.example.questionshare.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.questionshare.R
+import kotlinx.android.synthetic.main.activity_question_view.*
 
 class QuestionView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_view)
+        bindListeners()
+    }
+    fun bindListeners(){
+        goto_cq.setOnClickListener {
+            val intent=Intent(this,CreativeQuestionView::class.java)
+            startActivity(intent)
+        }
+
+        goto_mcq.setOnClickListener {
+            val intent=Intent(this,MultipleChoiceQuestionView::class.java)
+            startActivity(intent)
+        }
     }
 }
