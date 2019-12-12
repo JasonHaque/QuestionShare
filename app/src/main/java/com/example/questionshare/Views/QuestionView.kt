6139c53d302704de72f2868112efc617edf0f8c8
JasonHaque <zahinjason220434@gmail.com>
2019-12-12 14:55:59 +0600

    added question view

---
+++ app/src/main/java/com/example/questionshare/Views/QuestionView.kt
@@ -0,0 +1,13 @@
+package com.example.questionshare.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.questionshare.R
+
+class QuestionView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_view)
+    }
+}
