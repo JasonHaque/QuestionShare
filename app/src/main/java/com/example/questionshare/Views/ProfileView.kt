28dfb1e2e65289db65a3a01e3ecfd8bcc6359d30
JasonHaque <zahinjason220434@gmail.com>
2019-12-12 18:28:20 +0600

    added log out method

--- app/src/main/java/com/example/questionshare/Views/ProfileView.kt
+++ app/src/main/java/com/example/questionshare/Views/ProfileView.kt
@@ -3,7 +3,9 @@ package com.example.questionshare.Views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.questionshare.Activities.LoginActivity
 import com.example.questionshare.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile_view.*
 
 class ProfileView : AppCompatActivity() {
@@ -16,6 +18,11 @@ class ProfileView : AppCompatActivity() {
     fun bindListeners(){
         goto_question_view.setOnClickListener {
             val intent=Intent(this,QuestionView::class.java)
+            startActivity(intent)
+        }
+        log_out.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent=Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
     }
