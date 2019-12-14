ce3662a0012bd8ae0f390b0b257119b993d43817
JasonHaque <zahinjason220434@gmail.com>
2019-12-14 13:27:43 +0600

    added new routes

--- app/src/main/java/com/example/questionshare/Admin/SetQuestion.kt
+++ app/src/main/java/com/example/questionshare/Admin/SetQuestion.kt
@@ -1,13 +1,27 @@
 package com.example.questionshare.Admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.questionshare.R
+import kotlinx.android.synthetic.main.activity_set_question.*
 
 class SetQuestion : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_question)
+        bindWigets()
+    }
+
+    fun bindWigets(){
+        type_mcq.setOnClickListener {
+            val intent =Intent(this,CreateMCQ::class.java)
+            startActivity(intent)
+        }
+        type_cq.setOnClickListener {
+            val intent = Intent(this,CreateCQ::class.java)
+            startActivity(intent)
+        }
     }
 }
